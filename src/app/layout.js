c721b60d19206c02d61a7221d9fc5118d6501a24
Joe Gregson <joegregson@icloud.com>
2023-05-13 15:40:46 +0100

    create and style Home Header componenet

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata = {
   title: "JG- google Clone",
   description: "Learn Next 13 2.0",
@@ -10,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+import HomeHeader from "@/components/HomeHeader";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+      <HomeHeader />
       <h1>gOOGLE cLONE</h1>
       <h1>hello world</h1>
     </main>
