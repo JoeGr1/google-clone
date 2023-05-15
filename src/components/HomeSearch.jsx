a634f37a64a764e30198794f954e13f9d518458d
Joe Gregson <joegregson@icloud.com>
2023-05-15 17:34:03 +0100

    addd working home buttons

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,17 +1,67 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { BsSearch, BsFillMicFill } from "react-icons/bs";
 
 const HomeSearch = () => {
+  const [input, setInput] = useState("");
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  };
+
+  const handleRandom = async (e) => {
+    setRandomSearchLoading(true);
+    const response = await fetch(`https://random-word-api.herokuapp.com/word`)
+      .then((res) => res.json())
+      .then((data) => data[0]);
+    if (!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoading(false);
+  };
+
   return (
     <>
-      <form className="flex w-full m5-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full m5-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl"
+      >
         <BsSearch className="text-xl text-gray-500 mr-3" />
-        <input type="text" className="flex-grow focus:outline-none" />
+        <input
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          type="text"
+          className="flex-grow focus:outline-none"
+        />
         <BsFillMicFill className="text-lg" />
       </form>
 
-      <div className="">
-        <button className="btn">Google Search</button>
-        <button className="btn">I'm Feeling Lucky</button>
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
+        <button onClick={handleSubmit} className="btn">
+          Google Search
+        </button>
+        <button
+          disabled={randomSearchLoading}
+          onClick={handleRandom}
+          className="btn flex items-center justify-center disabled:opacity-80"
+        >
+          {randomSearchLoading ? (
+            <img
+              src="Spinner.svg"
+              alt="loading..."
+              className="h-6 text-center"
+            />
+          ) : (
+            "I am Feeling Lucky"
+          )}
+        </button>
       </div>
     </>
   );
